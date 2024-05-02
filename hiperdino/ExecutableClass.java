c5d20d61ae1bd68f5cccef0086a82e57ec7cc068
Marco Gil Flores <159430116+ElMafiosoSalesiano@users.noreply.github.com>
2024-05-02 17:53:40 +0100

    features(random products): Method was finished
    The method "products" can return now one random product of the list.

---
+++ hiperdino/ExecutableClass.java
@@ -0,0 +1,9 @@
+package hiperdino;
+
+import hiperdino.products.Products;
+
+public class ExecutableClass {
+    public static void main(String[] args) {
+        System.out.println(Products.products());
+    }
+}
