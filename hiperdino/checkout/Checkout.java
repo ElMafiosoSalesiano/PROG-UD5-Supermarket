08cb01c0dfcf335c22936c7c038f6d99190991dc
Marco Gil Flores <159430116+ElMafiosoSalesiano@users.noreply.github.com>
2024-05-03 17:43:45 +0100

    build(Checkout): Queque was implemented
    Now queque was implemented on "Checkout" class

---
+++ hiperdino/checkout/Checkout.java
@@ -0,0 +1,20 @@
+package hiperdino.checkout;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import hiperdino.client.Client;
+
+public class Checkout {
+
+    private int checkout = 1;
+    
+    public static void checkout(){
+    
+    Queue<Client> clients = new LinkedList<>();
+    }
+
+
+
+
+}
