323a25ed90cabd9bd86a6df26d1798095f69ee3b
Marco Gil Flores <159430116+ElMafiosoSalesiano@users.noreply.github.com>
2024-05-03 19:21:56 +0100

    build(addClient): add client method is avaible
    Now client method is avaible

--- hiperdino/checkout/Checkout.java
+++ hiperdino/checkout/Checkout.java
@@ -4,17 +4,20 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 import hiperdino.client.Client;
+import hiperdino.people.People;
+import hiperdino.products.Products;
 
 public class Checkout {
-
+    private static Queue<Client> clientes = new LinkedList<>();
     private int checkout = 1;
     
-    public static void checkout(){
-    
-    Queue<Client> clients = new LinkedList<>();
+    public static void addClient(){
+        Client cliente = new Client(Products.randomBuyList(), People.people());
+        clientes.add(cliente);
+        System.out.println("cliente añadido satisfactoriamente");
     }
 
-
+    
 
 
 }
